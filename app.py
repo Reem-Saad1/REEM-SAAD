1e73a05bae9059f2dc2c0aad7893c1a677d72681
Reem-Saad1 <162417669+Reem-Saad1@users.noreply.github.com>
2024-03-21 12:52:21 +0000

    receive message and save to db

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
-from database import load_projects_from_db
-app=Flask(__name__)
+from flask import Flask, render_template,request
+from database import load_projects_from_db, load_project_from_db,add_massage_to_db
 
+app=Flask(__name__)
 
 @app.route('/')#الصفحه الرئيسية 
 def hello_world():
@@ -9,6 +9,21 @@ def hello_world():
   return render_template("home.html",myskills=projects,
                         myname = 'Reem')
 #first class applecktion 
+
+@app.route('/project/<id>')
+def show_project(id):
+  project=load_project_from_db(id)
+  return render_template("projectsdetalis.html",project=project)
+
+@app.route("/contacus")
+def contactpage():
+  return render_template("contacus.html")
+
+@app.route("/massage",methods=['POST'])
+def massageme():
+  data = request.form
+  add_massage_to_db(data)
+  return render_template("massage_submitted.html",massage=data)
 
 if __name__=='__main__':
    app.run(host="0.0.0.0",port=True)#run flask server 
--- database.py
+++ database.py
@@ -3,9 +3,7 @@ import os
 
 db_connection_string=os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(db_connection_string,
-    
-)
+engine = create_engine(db_connection_string)
 
 def load_projects_from_db():
   with engine.connect() as conn:
@@ -14,4 +12,20 @@ def load_projects_from_db():
     for row in result.all():
        myprojects.append(dict(row._mapping))
     return myprojects
-      
+
+def load_project_from_db(id):
+  with engine.connect() as conn:
+   result = conn.execute(text(f"select * from myprojects where id = {id} "))
+
+  rows= result.all()
+  return dict(rows[0]._mapping)
+
+def add_massage_to_db(data):
+  with engine.connect()as conn:
+    sql = text(
+      f"INSERT INTO massages (full_name, email, massage) VALUES (\'{data['full_name']}\',\'{data['email']}\',\'{data['massage']}\')"
+      )
+
+    print(sql)
+    conn.execute(sql)
+    conn.commit()
